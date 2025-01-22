70606061d4d43c78425478f99ff72887cc936d04
alexketh <alexketh.ca@gmail.com>
2025-01-21 20:52:53 -0800

    Setting up configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+      title: "Simple Weather App",
+    },
+  },
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['@/assets/css/main.scss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  runtimeConfig: {
+    public: {
+      weatherApiKey: process.env.NUXT_PUBLIC_WEATHER_API_KEY
+    }
+  },
 })
